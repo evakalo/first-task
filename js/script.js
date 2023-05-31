e923c673afedfbca4e319c1d65fb588b0ffaf44d
evakalo <99054186+evakalo@users.noreply.github.com>
2023-05-31 14:26:33 +0200

    added the toggle function
    changed the toggle function so the text appears on button click , and disapears when button is clicked again

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 const button = document.querySelector(".button");
 function openText() {
   let text = document.querySelector("p");
-  let headline = document.querySelector("h1");
   text.classList.toggle("animate-paragraph");
-  headline.classList.toggle("animate-header");
 }
 
 button.addEventListener("click", openText);
